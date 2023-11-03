7135f76255f8f7f50ed389d7c404ec3e3279273d
liwuyi <1666071726@qq.com>
2023-11-03 15:48:29 +0800

    Initial commit

---
+++ src/main/java/com/example/concurrenttest/concurrent/ConcurrencyTest.java
@@ -0,0 +1,29 @@
+package com.example.concurrenttest.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Sl
+public class ConcurrencyTest {
+    public static int clientTotal = 5000;
+    public static int threadTotal = 200;
+    public static int count=0;
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService= Executors.newCachedThreadPool();
+        final Semaphore semaphore=new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch=new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(()->{
+                try{
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                }catch (Exception e){
+                    log
+                }
+            });
+        }
+    }
+}
---
+++ src/main/java/com/example/concurrenttest/concurrent/package-info.java
@@ -0,0 +1,1 @@
+package com.example.concurrenttest.concurrent;
