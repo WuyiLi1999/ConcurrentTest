54bb3c9f9891cf960121dbd6319e807683d962dc
liwuyi <1666071726@qq.com>
2023-11-03 15:50:18 +0800

    并发编程测试类

--- src/main/java/com/example/concurrenttest/concurrent/ConcurrencyTest.java
+++ src/main/java/com/example/concurrenttest/concurrent/ConcurrencyTest.java
@@ -5,7 +5,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
-@Sl
 public class ConcurrencyTest {
     public static int clientTotal = 5000;
     public static int threadTotal = 200;
